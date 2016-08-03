6691c48def6f76b42826a60471c393d84da5fe0d
李浩 <LeeHow@lihaodemacbook-pro.local>
2016-08-03 19:59:04 +0800

    初始化

---
+++ GitStudy/GitStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitStudy
+//
+//  Created by 李浩 on 16/8/3.
+//  Copyright © 2016年 李浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
