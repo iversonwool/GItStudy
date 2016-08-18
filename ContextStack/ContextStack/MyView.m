6e08ec9e223278f57e95041e12359647406ca6d9
李浩 <LeeHow@lihaodemacbook-pro.local>
2016-08-18 14:58:01 +0800

    图形的矩阵变换

--- ContextStack/ContextStack/MyView.m
+++ ContextStack/ContextStack/MyView.m
@@ -15,7 +15,32 @@
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
+    CGContextRef currentCtx = UIGraphicsGetCurrentContext();
+#warning 路径一定要放在上下文矩阵操作之后
     
+    // 平移变换
+    CGContextTranslateCTM(currentCtx, 30, 50);
+    
+    // 旋转
+    CGContextRotateCTM(currentCtx, M_PI_4);
+    
+    // 缩放
+    CGContextScaleCTM(currentCtx, 0.6, 1);
+    
+    UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(-10, -30, 100, 60)];
+    
+    CGContextAddPath(currentCtx, path.CGPath);
+    
+    [[UIColor redColor] setFill];
+    
+    CGContextFillPath(currentCtx);
+    
+    
+
+}
+
+
+- (void)contextStack {
     CGContextRef currentCtx = UIGraphicsGetCurrentContext();
     
     // 先保存到图形上下文栈
@@ -38,18 +63,17 @@
     UIBezierPath *path1 = [UIBezierPath bezierPath];
     [path1 moveToPoint:CGPointMake(120, 10)];
     [path1 addLineToPoint:CGPointMake(120, 100)];
- 
+    
     CGContextAddPath(currentCtx, path1.CGPath);
     
     
     // 再取出栈顶
     CGContextRestoreGState(currentCtx);
-//    [[UIColor blackColor] setStroke];
-//    CGContextSetLineWidth(currentCtx, 1);
-//    CGContextSetLineCap(currentCtx, kCGLineCapButt);
+    //    [[UIColor blackColor] setStroke];
+    //    CGContextSetLineWidth(currentCtx, 1);
+    //    CGContextSetLineCap(currentCtx, kCGLineCapButt);
     
     CGContextStrokePath(currentCtx);
-
 }
 
 
